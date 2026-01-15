ef4a16f34765322cc6578f1a7bd65464111eeb2a
최현수 <106013493+smuHyuns@users.noreply.github.com>
2026-01-15 23:00:46 +0900

    refact : 테스트컨트롤러 수정

--- review_server/src/main/java/reviewbot/review_server/controller/TestController.java
+++ review_server/src/main/java/reviewbot/review_server/controller/TestController.java
@@ -3,7 +3,9 @@ package reviewbot.review_server.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("/test")
 public class TestController {
     @GetMapping
