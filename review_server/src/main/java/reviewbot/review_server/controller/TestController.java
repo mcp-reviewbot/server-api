de3802c73501fda0bb8d4b4644886202ff6d2b7e
최현수 <106013493+smuHyuns@users.noreply.github.com>
2026-01-25 12:19:58 +0900

    feat : 테스트용 코드 구축

--- review_server/src/main/java/reviewbot/review_server/controller/TestController.java
+++ review_server/src/main/java/reviewbot/review_server/controller/TestController.java
@@ -2,6 +2,7 @@ package reviewbot.review_server.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping
     public ResponseEntity<String> test() {
-        return ResponseEntity.ok("테스트 성공!");
+        return ResponseEntity.ok("테스트 성공!!");
+    }
+
+    @PostMapping("/for")
+    public ResponseEntity<String> testFor() {
+        System.out.println("이것은 웹훅이 제대로 날라가는지에 대한 테스트 코드입니다.");
+        return ResponseEntity.ok("킬킬");
     }
 }
